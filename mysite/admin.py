fd4cbf8343218ce28c2fc370cc25219ab19a2cd0
yogeshnile <yogeshnile786@gmail.com>
2020-10-24 19:59:16 +0530

    Admin panel developed

--- mysite/admin.py
+++ mysite/admin.py
@@ -2,7 +2,25 @@ from django.contrib import admin
 from .models import Course, Lecture, Section, LectureComment
 
 # Register your models here.
-admin.site.register(Course)
-admin.site.register(Section)
-admin.site.register(Lecture)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title','course_type')
+    search_fields = ('title',)
+    list_filter = ('course_type',)
+    list_per_page = 10
+
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course',)
+    list_per_page = 20
+    search_fields = ('title',)
+    list_filter = ('course',)
+    
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'section', 'lecture_type',)
+    list_filter = ('lecture_type', 'course',)
+    list_per_page = 50
+    search_fields = ('title','section',)
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(Lecture, LectureAdmin)
 admin.site.register(LectureComment)
