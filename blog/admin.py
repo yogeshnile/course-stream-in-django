4053f4cd3b02c28b677f92362e58384355e1e222
yogeshnile <yogeshnile786@gmail.com>
2020-10-25 09:45:58 +0530

    admin table structure

--- blog/admin.py
+++ blog/admin.py
@@ -7,5 +7,11 @@ class PostAdmin(admin.ModelAdmin):
     list_per_page = 20
     search_fields = ('title',)
 
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display = ('comment','post','user','timestamp')
+    list_per_page = 20
+    search_fields = ('comment',)
+    list_filter = ('post',)
+
 admin.site.register(Post, PostAdmin)
-admin.site.register(BlogComment)
+admin.site.register(BlogComment, BlogCommentAdmin)
--- student/admin.py
+++ student/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import StudentInfo
 
 # Register your models here.
-admin.site.register(StudentInfo)
+class StudentInfoAdmin(admin.ModelAdmin):
+    list_display = ('username','mobile_no','dob','address')
+    list_per_page = 20
+    search_fields = ('username','mobile_no','address')
+
+admin.site.register(StudentInfo, StudentInfoAdmin)
