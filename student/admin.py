c619bcc5c38b3363379e22384eb22376313a1ac0
yogeshnile <yogeshnile786@gmail.com>
2020-11-02 17:13:49 +0530

    Many to Many relationship added

--- student/admin.py
+++ student/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import StudentInfo
+from .models import StudentInfo, CourseSubscription
 
 # Register your models here.
 class StudentInfoAdmin(admin.ModelAdmin):
     list_display = ('username','mobile_no','dob','address')
     list_per_page = 20
     search_fields = ('username','mobile_no','address')
+    readonly_fields = ('username','mobile_no','dob','address')
+
+class CourseSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('student','course')
+    list_per_page = 20
+    search_fields = ('student','course')
+    readonly_fields = ('DateStamp',)
 
 admin.site.register(StudentInfo, StudentInfoAdmin)
+admin.site.register(CourseSubscription, CourseSubscriptionAdmin)
--- student/models.py
+++ student/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from mysite.models import Course
+from django.utils.timezone import now
 
 # Create your models here.
 class StudentInfo(models.Model):
@@ -10,3 +12,11 @@ class StudentInfo(models.Model):
 
     def __str__(self):
         return f"{self.username} - {self.username.email} - {self.mobile_no}"
+
+class CourseSubscription(models.Model):
+    student = models.OneToOneField("student.StudentInfo", verbose_name=("username"), on_delete=models.CASCADE)
+    course = models.OneToOneField("mysite.Course", verbose_name=("title"), on_delete=models.CASCADE)
+    DateStamp = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.student.username} ==== {self.course}"
