9f67ea3c9f57cbc749a7e0df6840d7c21370116c
yogeshnile <yogeshnile786@gmail.com>
2020-11-08 14:01:29 +0530

    Security Enhancement

--- mysite/views.py
+++ mysite/views.py
@@ -17,37 +17,43 @@ def courses(request):
     return render(request, 'course/courses.html', context)
 
 def course_detail(request, slug):
-    course = Course.objects.filter(course_slug=slug).first()
-    section = Section.objects.filter(course=course)
-    lecture = Lecture.objects.filter(course=course)
-    if request.user.is_authenticated == True:
-        subscription_course = CourseSubscription.objects.filter(student=StudentInfo.objects.filter(username=request.user).first(), 
-        course=course).first()
+    if not Course.objects.filter(course_slug=slug).exists():
+        return render(request, '404.html')
     else:
-        subscription_course = None
-    context = {
-        "course":course,
-        "section":section,
-        "lecture":lecture,
-        "subscription_course":subscription_course,
-    }
-    return render(request, 'course/course_detail.html', context)
+        course = Course.objects.filter(course_slug=slug).first()
+        section = Section.objects.filter(course=course)
+        lecture = Lecture.objects.filter(course=course)
+        if request.user.is_authenticated == True:
+            subscription_course = CourseSubscription.objects.filter(student=StudentInfo.objects.filter(username=request.user).first(), 
+            course=course).first()
+        else:
+            subscription_course = None
+        context = {
+            "course":course,
+            "section":section,
+            "lecture":lecture,
+            "subscription_course":subscription_course,
+        }
+        return render(request, 'course/course_detail.html', context)
 
 def lecture_detail(request, slug, lecture_slug):
-    course = Course.objects.filter(course_slug=slug).first()
-    section = Section.objects.filter(course=course)
-    lecture = Lecture.objects.filter(course=course)
-    video = Lecture.objects.filter(lecture_slug=lecture_slug).first()
-    Lecture_Comment = LectureComment.objects.filter(lecture=video)
+    if not Course.objects.filter(course_slug=slug).exists() or not Lecture.objects.filter(lecture_slug=lecture_slug).exists():
+        return render(request, '404.html')
+    else:
+        course = Course.objects.filter(course_slug=slug).first()
+        section = Section.objects.filter(course=course)
+        lecture = Lecture.objects.filter(course=course)
+        video = Lecture.objects.filter(lecture_slug=lecture_slug).first()
+        Lecture_Comment = LectureComment.objects.filter(lecture=video)
 
-    context ={
-        "course":course,
-        "section":section,
-        "lecture":lecture,
-        "video":video,
-        "lecture_comment":Lecture_Comment,
-    }
-    return render(request, 'course/lecture.html', context)
+        context ={
+            "course":course,
+            "section":section,
+            "lecture":lecture,
+            "video":video,
+            "lecture_comment":Lecture_Comment,
+        }
+        return render(request, 'course/lecture.html', context)
 
 def pricing(request):
     course = Course.objects.filter(course_type="PAID")
@@ -70,24 +76,28 @@ def videoComment(request):
     return redirect('home')
 
 def Checkout(request, slug):
-    if request.user.is_authenticated == True:
-        course = Course.objects.filter(course_slug = slug).first()
-        if course.course_price == 0:
-            context = {
-                "course":course,
-            }
-        else:
-            with open("secret key.json",'r') as secret:
-                key = json.load(secret)['razorpay']
-            student = StudentInfo.objects.filter(username=request.user).first()
-            client = razorpay.Client(auth=(key['key id'],key['key secret']))
-            payment_id = client.order.create({'amount':course.course_price*100, 'currency':'INR','payment_capture':'1'})
-            new_payment = PaymentProcess(student=student, course=course, order_id=payment_id['id'])
-            new_payment.save()
-            context = {
-                "course":course,
-                "payment":payment_id,
-                "student":student,
-            }
-        return render(request, 'course/checkout.html', context)
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    if not Course.objects.filter(course_slug = slug).exists():
+        return render(request, '404.html')
+    else:
+        if request.user.is_authenticated == True:
+            course = Course.objects.filter(course_slug = slug).first()
+            if course.course_price == 0:
+                context = {
+                    "course":course,
+                }
+            else:
+                with open("secret key.json",'r') as secret:
+                    key = json.load(secret)['razorpay']
+                student = StudentInfo.objects.filter(username=request.user).first()
+                client = razorpay.Client(auth=(key['key id'],key['key secret']))
+                payment_id = client.order.create({'amount':course.course_price*100, 'currency':'INR','payment_capture':'1'})
+                new_payment = PaymentProcess(student=student, course=course, order_id=payment_id['id'])
+                new_payment.save()
+                context = {
+                    "course":course,
+                    "payment":payment_id,
+                    "student":student,
+                    "key":key['key id'],
+                }
+            return render(request, 'course/checkout.html', context)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
